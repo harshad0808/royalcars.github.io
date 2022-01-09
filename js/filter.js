10ddd4c1d18cce1a5aeacf7dcfc957fab08bb42d
harshad0808 <62278752+harshad0808@users.noreply.github.com>
2022-01-09 12:53:37 +0530

    Update filter.js

--- js/filter.js
+++ js/filter.js
@@ -1,23 +1,30 @@
-function selectall() {
-    document.querySelector(".bmw").style.display = ""
-    document.querySelector(".jaguar").style.display = ""
-    document.querySelector(".audi").style.display = ""
-}
-function bmw() {
-    document.querySelector(".audi").style.display = "none"
-    document.querySelector(".jaguar").style.display = "none"
-    document.querySelector(".bmw").style.display = ""
-}
-function audi() {
-    document.querySelector(".bmw").style.display = "none"
-    document.querySelector(".jaguar").style.display = "none"
-    document.querySelector(".audi").style.display = ""
-}
-function jaguar() {
-    document.querySelector(".bmw").style.display = "none"
-    document.querySelector(".jaguar").style.display = ""
-    document.querySelector(".audi").style.display = "none"
-}
+function change(className, displayValue) {
+    var items = document.getElementsByClassName(className);
+    for (var i=0; i < items.length; i++) {
+      items[i].style.setProperty('display', displayValue, 'important');;
+      console.log(items[i].innerHTML)
+    }
+  }
+  function audi() {
+      change("bmw","none")
+      change("jaguar","none") 
+      change("audi","")
+  }
+  function jaguar() {
+      change("bmw","none")
+      change("jaguar","") 
+      change("audi","none")
+  }
+  function bmw() {
+      change("bmw","")
+      change("jaguar","none") 
+      change("audi","none")
+  }
+  function selectall() {
+      change("bmw","")
+      change("jaguar","") 
+      change("audi","")
+  }
 
 function testdrive(ref) {
     let a = ref.parentElement.parentElement.children[0];
